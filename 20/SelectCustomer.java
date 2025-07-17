16d7dc95a8616e99b64addd6eb2dac2db6bec07b
shinogith <s2112601@s.do-johodai.ac.jp>
2025-07-17 16:30:38 +0900

    Add files via upload

---
+++ 20/SelectCustomer.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import model.Customer;
+import dao.CustomerDAO;
+
+public class SelectCustomer {
+	public static void main(String[] args) {
+		DatabaseConnection con = new DatabaseConnection();
+
+		CustomerDAO customerDAO = new CustomerDAO(con.getConnection());
+		List<Customer> customerList = customerDAO.findAll();
+
+		for (Customer customer : customerList) {
+			System.out.println("顧客ID：" + customer.getId());
+			System.out.println("　氏名：" + customer.getName());
+		}
+
+		con.close();
+	}
+}
---
+++ 20_2/dao/CustomerDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.Customer;
+
+public class CustomerDAO {
+	      private Connection con;
+
+	      public CustomerDAO(Connection con){
+	      	         this.con=con;
+	      }
+	      	
+	      public List<Customer>findAll(){
+		             List<Customer>customerList=new ArrayList<>();
+	      	         try{
+	      	         	      String sql="SELECT* FROM customer";
+	      	         	      PreparedStatement pStmt=con.prepareStatement(sql);
+	      	         	      ResultSet rs=pStmt.executeQuery();
+	      	         	      while(rs.next()){
+	      	         	               int id=rs.getInt("customer_id");
+	      	         	               String name = rs.getString("customer_name");
+	      	         	               Customer customer = new Customer(id,name);
+	      	         	               customerList.add(customer);
+	      	         	      }
+	      	         }catch(SQLException e){
+	      	          	      e.printStackTrace();
+	      	         }
+	      	         return customerList;
+	      }
+	      public void insert(Customer customer){
+	      	         try{
+	      	         	      String sql="INSERT INTO customer(customer_id, customer_name) VALUES(?,?)";
+	      	         	      PreparedStatement pStmt=con.prepareStatement(sql);
+	      	         	      pStmt.setInt(1,customer.getId());
+	      	         	      pStmt.setString(2,customer.getName());
+	      	         	      pStmt.executeUpdate();
+	      	         }catch(SQLException e){
+	      	          	      e.printStackTrace();
+	      	         }
+	      }
+}	
